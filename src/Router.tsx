162c507417ffd20ac1d8bb22f031ce92ba040317
HealMon <71082215+RspSang@users.noreply.github.com>
2022-04-08 16:22:24 +0900

    Nested route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Coins from "./components/coins";
 import Coin from "./components/coin";
+import Chart from "./components/chart";
+import Price from "./components/price";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Coins />} />
-        <Route path="/:coinId" element={<Coin />} />
+        <Route path="/:coinId" element={<Coin />}>
+          <Route path="chart" element={<Chart />} />
+          <Route path="price" element={<Price />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
